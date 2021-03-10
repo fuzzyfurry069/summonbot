19fe6c07b10242a7d07440f713623662630ce2cd
fuzzyfurry069 <fuzzywilson21@gmail.com>
2021-03-10 13:42:13 -0600

    Changed Prefix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     token: "TokenHere", //Token of the bot
-    prefix: "/", //prefix of the bot
-    version: "1.1.1", //Current version
+    prefix: "-", //prefix of the bot
+    version: "1.0.0", //Current version
     footer: "Summon Bot | V1.0", //Default Footer, changing this to copyright will result in a DMCA Complaint.
 
     owner: {
