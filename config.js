44a3aa931bb0253b69fa687b0f148bf3f08255c9
fuzzyfurry069 <fuzzywilson21@gmail.com>
2021-03-10 13:35:53 -0600

    Added source information

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    token: "TokenHere", //Token of the bot
+    prefix: "/", //prefix of the bot
+    version: "1.1.1", //Current version
+    footer: "Summon Bot | V1.0", //Default Footer, changing this to copyright will result in a DMCA Complaint.
+
+    owner: {
+        name: "fuzzyfurry069#0420",
+        id: "399436902475759626",
+    },
+
+    sql: {
+        host: "localhost",
+        user: "root",
+        database: "database name here",
+        password: "Password Here",
+    },
+}
